52482442c8578b04b54941005eda4a808f72d81e
DerekB13 <derekb@live.ca>
2017-04-20 14:56:02 -0300

    Add files via upload

---
+++ BackButton.java
@@ -0,0 +1,14 @@
+package crazyeights.gui;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+
+public class BackButton extends Button {
+
+	public BackButton(){
+		setMaxSize(110, 90);
+		setPadding(new Insets(35));
+		getStyleClass().remove(0);
+		getStyleClass().add("backButton");
+	}
+}
